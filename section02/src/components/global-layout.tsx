cca7d3daf8162c5cbf97e6e4c2ff426d39777c5a
KH2Wone <kkhw1202@gmail.com>
2024-12-15 21:27:45 +0100

    css module 적용 및 컴포넌트 분리, searchbar 생성

---
+++ section02/src/components/global-layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from "react";
+import Link from "next/link";
+import style from './global-layout.module.css';
+
+export default function GlobalLayout({children}: { children: ReactNode }) {
+    return <div className={style.container}>
+        <header className={style.header}><Link href={'/'}>📚ONEBITE BOOKS</Link></header>
+        <main className={style.main}>{children}</main>
+        <footer className={style.footer}>제작 @강희원</footer>
+    </div>
+}
