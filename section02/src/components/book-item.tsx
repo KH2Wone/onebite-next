6e7d142a0681f55ede2f6f452a3866e37ac867f3
KangHeeWon <kkhw1202@gmail.com>
2024-12-22 01:20:09 +0900

    페이지별 UI 구현

--- section02/src/components/book-item.tsx
+++ section02/src/components/book-item.tsx
@@ -1,5 +1,6 @@
 import {BookData} from "@/types";
 import Link from "next/link";
+import style from './book-item.module.css'
 
 export default function BookItem({
                                      id,
@@ -10,8 +11,13 @@ export default function BookItem({
                                      publisher,
                                      coverImgUrl
                                  }: BookData) {
-    return <Link href={`/book/${id}`}>
+    return <Link href={`/book/${id}`} className={style.container}>
         <img src={coverImgUrl} alt={title}/>
-
+        <div>
+            <div className={style.title}>{title}</div>
+            <div className={style.subTitle}>{subTitle}</div>
+            <br/>
+            <div className={style.author}>{author} | {publisher}</div>
+        </div>
     </Link>
 }
