25d4b00c1a985ac9a8058a6bcba2e8fa29fa1420
KH2Wone <kkhw1202@gmail.com>
2024-12-27 01:36:21 +0900

    [App Router] 네비게이팅 원리 및 서버컴포넌트 이해하기

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +26,14 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <header>
+        <Link href={'/'}>index</Link>
+        &nbsp;
+        <Link href={'/search'}>search</Link>
+        &nbsp;
+        <Link href={'/book/1'}>book/1</Link>
+      </header>
+      {children}
       </body>
     </html>
   );
